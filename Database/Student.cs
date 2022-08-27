c6f93068686f039ae9bc32631f4bd1ff074c9a61
M1kee <michael.nunezb@outlook.com>
2022-08-26 22:26:25 -0400

    add not mapped field in student

--- Database/Student.cs
+++ Database/Student.cs
@@ -1,13 +1,14 @@
-// using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EFCoreDemo.Database;
 
 public class Student : BaseEntity {
     public string Name { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
-
-     //[NotMapped]
     public string FullName => $"{Name} {LastName}";
+
+    [NotMapped]
+    public string RUT { get; set; }
 
     public List<Course> Courses { get; set; }
     public List<Grade> Grades { get; set; }
